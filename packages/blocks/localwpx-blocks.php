598a469efa7b52d5050be95dc75987bc7942943f
Eric Phillips <github@nekvtcode.com>
2023-04-15 07:46:27 -0400

    Add example local plugin package

---
+++ packages/blocks/localwpx-blocks.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: LocalWPX Blocks
+ * Description: A locally installed plugin package.
+ * Version: 1.0.0
+ * Author: Eric Phillips
+ *
+ * @package localwpx/localwpx-blocks
+ */
